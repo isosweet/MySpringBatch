2c70d069836980f25eeed5946a78baf4b05ab551
isosweet <405438526@qq.com>
2017-04-10 13:57:44 +0800

    MySpringBatch-Job

---
+++ MySpringBatch-Job/src/main/java/com/myspringbatch/job/MySpringBatch_Job/App.java
@@ -0,0 +1,13 @@
+package com.myspringbatch.job.MySpringBatch_Job;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
